878e63163ef216836c51b8e3be78e9982a1deeac
Maneth-ii <manethbandara25@gmail.com>
2023-12-13 22:15:42 +0530

    init

---
+++ src/third.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+class Frame extends JFrame{
+    Frame(){
+
+    }
+}
+
+public class third {
+    public static void main(String[] args){
+        Frame frame = new Frame();
+        frame.setVisible(true);
+        frame.getContentPane().setBackground(Color.DARK_GRAY);
+        frame.setBounds(0,0,400,400);
+
+    }
+}
